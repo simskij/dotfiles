13f11a832acc7a5843a97199e32387989e1cd0e2
Simon Aronsson <simme@arcticbit.se>
2023-05-12 10:45:42 +0200

    delta

---
+++ rcm/config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- 
+-- Load lua modules
+--
+
+require 'init'
+require 'globals'
+require 'keybindings'
+require 'extensions'
+require 'gutter'
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
