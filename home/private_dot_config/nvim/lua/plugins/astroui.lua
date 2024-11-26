846199dd0ae2d77ee9aa37d0e5cd0539cf3f29f8
Simon Aronsson <simme@arcticbit.se>
2024-11-26 09:28:57 +0100

    Add home/.config/nvim/README.md Add home/.config/nvim/.neoconf.json Add home/.config/nvim/.stylua.toml Add home/.config/nvim/lua/community.lua Add home/.config/nvim/lua/lazy_setup.lua Add home/.config/nvim/lua/plugins/astrocore.lua Add home/.config/nvim/lua/plugins/astrolsp.lua Add home/.config/nvim/lua/plugins/astroui.lua Add home/.config/nvim/lua/plugins/mason.lua Add home/.config/nvim/lua/plugins/none-ls.lua Add home/.config/nvim/lua/plugins/treesitter.lua Add home/.config/nvim/lua/plugins/user.lua Add home/.config/nvim/lua/polish.lua Add home/.config/nvim/neovim.yml Add home/.config/nvim/selene.toml

---
+++ home/private_dot_config/nvim/lua/plugins/astroui.lua
@@ -0,0 +1,24 @@
+---@type LazySpec
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    colorscheme = "catppuccin-frappe",
+    highlights = {
+      init = {},
+      astrodark = {},
+    },
+    icons = {
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
+  },
+}
---
+++ home/private_dot_config/nvim/lua/polish.lua
@@ -0,0 +1,7 @@
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- require("notify").setup({
+--   background_color = "#1a1b26",
+-- })
