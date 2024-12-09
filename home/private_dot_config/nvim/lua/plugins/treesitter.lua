27e8f0519af50fd09736825f93b6369e25e6aac2
Simon Aronsson <simme@arcticbit.se>
2024-12-09 11:40:31 +0100

    Update home/.config/hypr/config.d/appearance.conf Update home/.config/hypr/config.d/layout.conf Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/plugins/treesitter.lua Update home/.config/nvim/lua/plugins/user.lua Update home/.config/waybar/config Update home/.config/waybar/style.css

--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  commit = "cfc6f2c117aaaa82f19bcce44deec2c194d900ab", -- v0.9.3, for nvim 0.9.2+
   opts = {
     ensure_installed = {
       "lua",
       "vim",
+      "markdown",
       -- add more arguments for adding more treesitter parsers
     },
+    auto_install = true,
   },
 }
